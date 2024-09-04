62a4a00a5e82dd2c44a1a2b7f79f6ee90208d57e
WojtekB <w_bielat@tlen.pl>
2024-09-04 17:43:16 +0100

    ddd

--- grzesiek_pyth_dz02.py
+++ grzesiek_pyth_dz02.py
@@ -4,7 +4,16 @@
 
 # inpput request for 6 numbers 1-49
 # input request for variant of the algorithm
-#
+# 
+# - wczytanie listy
+# - wczytanie/zmierzenie długości listy
+# - wczytanie (pierszego) wyrazu listy
+# - wczytanie następnego wyrazu listy
+# - opcje::
+#     a) zapisanie pierwszego wczytanego wyrazu do nowej tablicy, porównanie wyrazów - gdy 1. większy: zapisanie w nowej tablicy z tyłu, - gdy mniejszy: zapisanie do nowej tablicy z tyłu.
+#     b) po porównaniu wyrazów - jeśli wyraz 1. większy od 2.: zapisanie go na końcu tablicy i skasowanie z przodu, jeśli mniejszy: przejście do następnego wyrazu tabllicy
+# OBA 'ALGORYTMY' SĄ FATALNE, ale to pierwsze, robocze pomysły na algorytm sortujący, bo nie chciało mi się czytać, jakie algorytmy już istnieją
+# 
 # display the numbers, sorted
 
 print('a remark thaat at least \'something\' has been done.')
